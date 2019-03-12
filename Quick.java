2219ccf89b74edd9d040d5f341ce73f5e467fcf6
Alvi Aziz <aaziz00@stuy.edu>
2019-03-11 23:01:55 -0400

    optimized quickselect and driver provides a little more info

--- Quick.java
+++ Quick.java
@@ -1,6 +1,20 @@
 public class Quick{
   public static int quickselect(int[] data, int k){
+    /*
     while (partition(data,0,data.length-1) != k){
+    }
+    return data[k];
+    */
+    return quicksel(data,k,0,data.length-1);
+  }
+
+  private static int quicksel(int[] data, int k, int start, int end){
+    int pivot = partition(data,start,end);
+    if (pivot < k){
+      return quicksel(data,k,pivot+1,end);
+    }
+    else if (pivot > k){
+      return quicksel(data,k,start,pivot-1);
     }
     return data[k];
   }
--- driver.java
+++ driver.java
@@ -2,11 +2,13 @@ public class driver{
   public static void main(String[] args){
     //TESTS PARTITION
     System.out.println("\nTESTING PARTITION");
-    System.out.println("The number above the array should be in its index (so 0 should be above 0 _ _ _ _ _)");
+    System.out.println("The number above the array should be in its index (so 4 should be above modified: _ _ _ _ 4 _)");
     System.out.println("does not work if partition is not public, so either make it public or comment this out");
     System.out.println("-------------------");
     int[] data = new int[] {0,1,4,3,2,5};
     System.out.println(Quick.partition(data,0,5));
+    System.out.println("original: 0 1 4 3 2 5");
+    System.out.print("modified: ");
     for(int i = 0 ; i < data.length ; i++){
       System.out.print(data[i] + " ");
     }
@@ -14,6 +16,8 @@ public class driver{
 
     data = new int[] {5,4,3,2,1,0};
     System.out.println(Quick.partition(data,0,5));
+    System.out.println("original: 5 4 3 2 1 0");
+    System.out.print("modified: ");
     for(int i = 0 ; i < data.length ; i++){
       System.out.print(data[i] + " ");
     }
@@ -21,6 +25,8 @@ public class driver{
 
     data = new int[] {1,4,3,2,5,0};
     System.out.println(Quick.partition(data,0,5));
+    System.out.println("original: 1 4 3 2 5 0");
+    System.out.print("modified: ");
     for(int i = 0 ; i < data.length ; i++){
       System.out.print(data[i] + " ");
     }
